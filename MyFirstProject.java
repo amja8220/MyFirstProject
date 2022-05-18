b16d10400adbc169aaa1e85354802713aa786c46
amja8220 <amjath.ha@gmail.com>
2022-05-18 12:39:46 +0530

    just update

--- MyFirstProject.java
+++ MyFirstProject.java
@@ -2,6 +2,8 @@ public class MyFirstProject {
     public static void main(String[] args) {
         int a=1;
         int b=2;
+        int c=10;
         System.out.println(a+b);
+        System.out.println(c+a);
     }
 }
