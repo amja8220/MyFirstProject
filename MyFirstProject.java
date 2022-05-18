87f67a88ee3b31f05601f37a3fe2173fc5a68c8d
amja8220 <105770975+amja8220@users.noreply.github.com>
2022-05-18 13:05:57 +0530

    other change in a variable

--- MyFirstProject.java
+++ MyFirstProject.java
@@ -1,6 +1,6 @@
 public class MyFirstProject {
     public static void main(String[] args) {
-        int a=1;
+        int a=100;
         int b=20;
         int c=10;
         System.out.println(a+b);
