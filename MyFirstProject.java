5689b2811952b1379f6832c28b09b7271be200a7
amja8220 <105770975+amja8220@users.noreply.github.com>
2022-05-18 13:27:03 +0530

    de

--- MyFirstProject.java
+++
@@ -1,9 +0,0 @@
-public class MyFirstProject {
-    public static void main(String[] args) {
-        int a=100;
-        int b=20;
-        int c=10;int d=20;
-        System.out.println(a+b);
-        System.out.println(c+a+d);
-    }
-}
